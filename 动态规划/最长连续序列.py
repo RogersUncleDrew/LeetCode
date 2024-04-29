89a09b7bd7f098361d9d5600f30f03991a20c2e9
RogersUncleDrew <48075696+RogersUncleDrew@users.noreply.github.com>
2024-04-29 17:03:07 +0800

    Create 最长连续序列.py

---
+++ 动态规划/最长连续序列.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        nums = list(set(nums))
+        nums.sort()
+        n = len(nums)
+        dp = [1]*n
+        for i in range(1,n):
+            if nums[i] == nums[i-1]+1:
+                dp[i] = max(dp[i],dp[i-1]+1)
+        return max(dp)
